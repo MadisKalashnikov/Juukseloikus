1057b14fb871db8a1c078ddb5cc5d0b69f75b8a9
Madis <96111936+MadisKalashnikov@users.noreply.github.com>
2026-07-16 23:15:52 +0300

    Lehe algus

---
+++ script.js
@@ -0,0 +1,35 @@
+const menuButton = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".nav-links");
+
+menuButton.addEventListener("click", (e) => {
+    e.stopPropagation();
+    menu.classList.toggle("active");
+});
+
+
+// menüü sulgemine lingile vajutades
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("active");
+    });
+});
+
+
+// menüü sulgemine mujale vajutades
+document.addEventListener("click", (e) => {
+
+    if (!menu.contains(e.target) && !menuButton.contains(e.target)) {
+        menu.classList.remove("active");
+    }
+
+});
+
+
+// menüü sulgemine ESC klahviga
+document.addEventListener("keydown", (e) => {
+
+    if (e.key === "Escape") {
+        menu.classList.remove("active");
+    }
+
+});
